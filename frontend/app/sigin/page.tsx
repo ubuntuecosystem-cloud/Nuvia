ae002290dd552ff65ee87a291b0e26e52ed1f9bb
Ubuntuecosystem <ubuntuecosystem@gmail.com>
2026-07-31 18:22:16 -0400

    Update page.tsx

--- frontend/app/sigin/page.tsx
+++ frontend/app/sigin/page.tsx
@@ -1,6 +1,4 @@
-import Link from "next/link";
-
-export default function Home() {
+export default function SignIn() {
   return (
     <main
       style={{
@@ -8,56 +6,19 @@ export default function Home() {
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        fontFamily: "system-ui",
         background: "#0b0b0f",
         color: "white",
+        fontFamily: "system-ui",
       }}
     >
       <div style={{ textAlign: "center" }}>
         <h1 style={{ fontSize: 40 }}>
-          Entry System
+          Sign In
         </h1>
 
-        <p style={{ opacity: 0.7, marginTop: 10 }}>
+        <p style={{ opacity: 0.7 }}>
           Presence • Relationship • Continuity
         </p>
-
-        <div
-          style={{
-            marginTop: 30,
-            display: "flex",
-            gap: 10,
-            justifyContent: "center",
-          }}
-        >
-          <Link
-            href="/signin"
-            style={{
-              padding: "10px 16px",
-              borderRadius: 8,
-              border: "1px solid white",
-              background: "transparent",
-              color: "white",
-              textDecoration: "none",
-            }}
-          >
-            Sign In
-          </Link>
-
-          <Link
-            href="/signup"
-            style={{
-              padding: "10px 16px",
-              borderRadius: 8,
-              border: "none",
-              background: "white",
-              color: "black",
-              textDecoration: "none",
-            }}
-          >
-            Create Account
-          </Link>
-        </div>
       </div>
     </main>
   );
